ff723e394cb97dc13c4d0709a321a95ab3bfce0e
redrobin97 <dylon20325@gmail.com>
2023-09-27 20:13:12 -0400

    final

---
+++ block13.js
@@ -0,0 +1,11 @@
+let a=-3
+
+//if the input value is divisible by two then print will be even
+b = (a / 2)
+c = Math.ceil(a / 2)
+
+if (b === c) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
---
+++ index.js
@@ -0,0 +1,67 @@
+// let a = "I am a string";
+
+
+// if (a) {
+//     console.log("true")
+// }
+// else if (a === null) {
+//     console.log("The null value is falsy");
+// }
+// else if (a === false) {
+//     console.log("The boolean value false is falsy");
+// }
+
+// else if (a === undefined){
+//     console.log("undefined is falsy");
+// }
+// else if (a === 0){
+//     console.log("The number 0 is falsy (the only falsy number)");
+// }
+// else if ( a === ""){
+//     console.log("The empty string is falsy (the only falsy string)");
+// }
+
+
+// let a = 0;
+// let b = -5;
+
+// if ( (a + b) === 0 ){
+//     console.log("0 is equal to 0");
+// }
+// else if( (a + b) > 100){
+//     console.log("101 is greater than 100");
+// }
+// else if ( (a + b) > 0 ){
+//     console.log("97 is greater than 0");
+// }
+// else if( (a + b) > 100){
+//     console.log("101 is greater than 100");
+// }
+// else if ( (a + b) <= -1000 ){
+//     console.log("-1000 is a negative number");
+// }
+// else if( (a + b) < 0){
+//     console.log("-5 is a negative number");
+// }
+
+// let a = 1;
+// let b = 2;
+
+// if(a && b >= 5){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+let param1A = "five"
+let param1B = 5
+let param2A = "dog"
+let param2B = "dawg"
+
+if((param1A===param1B) || (param2A===param2B)){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
